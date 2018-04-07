8085e2e845b0649b90bdf7eea75afd52b9aebf48
dmitri ponomarjov <dmitri@artweb.ee>
2018-04-07 19:55:36 +0300

    File renames

--- example/example.php
+++ example/example.php
@@ -4,12 +4,12 @@ include_once('../src/ImageObject.php');
 include_once('../src/Filter.php');
 
 
-$imageProcess = new \ImageProcess\ImageProcess('/');
+$imageProcess = new \ImageProcess\ImageProcess(__DIR__ . '\\');
 $imageProcess->setImagesCaching(false);
-$imageProcess->registerImage('main', 'test.jpg');
+$imageProcess->registerImage('main', __DIR__ . '\test.jpg');
 
 $imageProcess->registerFilter('crop', 'width=100,height=50, valign=top, halign=center');
-$imageProcess->registerExport('main', 'png', '/test.png');
+$imageProcess->registerExport('main', 'png', __DIR__ . '\test.png');
 $imageProcess->executeProcess();
 
 echo 'done';
